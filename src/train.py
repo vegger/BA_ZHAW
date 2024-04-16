eed9b0c9b97b863d99258ad9e70e9b2a813a7e39
cyrilgabriele <cyril.gabriele@gmail.com>
2024-04-16 10:39:02 +0000

    CG: major advancement => can now train a first (dummy) model

--- src/train.py
+++ src/train.py
@@ -1,72 +1,192 @@
 import os
-import wandb
+# import wandb
+import torch
 import pytorch_lightning as pl
-from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
-from torch.utils.data import DataLoader, random_split
+# from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
+from pytorch_lightning.loggers import TensorBoardLogger
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, random_split
 from pytorch_lightning.callbacks import ModelCheckpoint
-# TODO: import model
-from Dataclasses.no_Stitchr.noStitchrDataset import NoStitchrDataset  # Define your dataset in this file
-from dotenv import load_dotenv
+from modelV1 import ModelV1
+from paired import Paired  
+# from dotenv import load_dotenv
 
 
-load_dotenv()
-experiment_description = "train.py test run"
-PROJECT_NAME = os.getenv("MAIN_PROJECT_NAME")
-wandb.init(project=PROJECT_NAME, job_type="f{experiment_description}")
-wandb.init()
+torch.manual_seed(42)
 
-# Download corresponding artifact (= dataset) from W&B
-# TODO: maybe implement a main() with arguments to pass which artifact to download
-dataset_name = "Paired_with_Negatives"
-artifact = wandb.use_artifact(f"{dataset_name}:latest")
-mcpastcr_table = artifact.get(f"{dataset_name}_table.table.json")
-dataset_dir = artifact.download()
-# Initialize dataset
-dataset = NoStitchrDataset(dataset_dir)
-print(f"type of dataset is: {type(dataset)}!\n")
-
-print(f"This is dataset: {dataset}\n")
-
-# Configuration
-batch_size = 32 # maybe increase? check w/ literature
-num_workers = 4
+# ---------------------------------------------------------------------------------
+# hyperparameters
+# ---------------------------------------------------------------------------------
+# NOT USED YET -----
+LEARNING_RATE = 5e-3
+BETA_1 = 0.9
+BETA_2 = 0.98
+EPSILON = 1e-9
+# NOT USED YET -----
 
 
-train_size = int(0.8 * len(dataset))
-val_size = len(dataset) - train_size
-train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+BATCH_SIZE = 32
+EPOCHS = 150
+# keep NUM_WORKERS = 0 as long as debugging
+NUM_WORKERS = 0
 
-'''
-# Initialize model
-model = YourModel()
-
-# Initialize loggers
-wandb_logger = WandbLogger(project="YourProjectName")
-tensorboard_logger = TensorBoardLogger("tb_logs", name="YourModelName")
-
-# Initialize a simple model checkpoint callback to save your models
-checkpoint_callback = ModelCheckpoint(
-    monitor='val_loss',
-    dirpath='./model_checkpoints',
-    filename='your_model_name-{epoch:02d}-{val_loss:.2f}',
-    save_top_k=3,
-    mode='min',
+# helper vars
+MODEL_OUT = 'model.pth'
+DEVICE = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
 )
 
-# Initialize the trainer
-trainer = pl.Trainer(
-    max_epochs=10,
-    logger=[wandb_logger, tensorboard_logger],
-    callbacks=[checkpoint_callback],
-    gpus=1 if torch.cuda.is_available() else 0,  # Adjust based on your setup
-)
 
-# Train the model
-trainer.fit(model, train_dataloader=train_loader, val_dataloaders=val_loader)
-
-# Optional: Close W&B run
-wandb_logger.experiment.finish()
 '''
+def setup(experiment_name: str, ): 
+    load_dotenv()
+    PROJECT_NAME = os.getenv("MAIN_PROJECT_NAME")
+    experiment_description = experiment_name
+    wandb.init(project=PROJECT_NAME, job_type=f"{experiment_description}")
+    wandb.init()
+'''
+
+# function used for the padding of the embeddings
+def pad_collate(batch):
+    epitope_embeddings, tra_cdr3_embeddings, trb_cdr3_embeddings, labels = [], [], [], []
+
+    for item in batch:
+        epitope_embeddings.append(item["epitope_embedding"])
+        tra_cdr3_embeddings.append(item["tra_cdr3_embedding"])
+        trb_cdr3_embeddings.append(item["trb_cdr3_embedding"])
+        labels.append(item["label"])
+
+    # as not fixed, manually looked up max_value
+    max_length = SEQ_MAX_LENGTH
+
+    # Function to pad embeddings to the maximum length
+    def pad_embeddings(embeddings):
+        print(f"type(embeddings[0]): {type(embeddings[0])}")
+        # print(f"embeddings[0].shape: {embeddings[0].shape}")
+
+        return torch.stack([
+            torch.nn.functional.pad(embedding, (0, 0, 0, max_length - embedding.size(0)), "constant", 0)
+            for embedding in embeddings
+        ])
+
+    # Pad and stack all embeddings and labels
+    epitope_embeddings = pad_embeddings(epitope_embeddings)
+    tra_cdr3_embeddings = pad_embeddings(tra_cdr3_embeddings)
+    trb_cdr3_embeddings = pad_embeddings(trb_cdr3_embeddings)
+    labels = torch.stack(labels)
+
+    # print(f"this is in pad_collate: epitope_embedding:\n{epitope_embeddings.shape}")
+    return {
+        "epitope_embedding": epitope_embeddings,
+        "tra_cdr3_embedding": tra_cdr3_embeddings,
+        "trb_cdr3_embedding": trb_cdr3_embeddings,
+        "label": labels
+    } 
+
+
+def main(): 
+    global SEQ_MAX_LENGTH
+
+    # experiment_name = "Experiment 1 - Test"
+    # setup(experiment_name=experiment_name)
+
+    
+    # -----------------------------------------------------------------------------
+    # data
+    # -----------------------------------------------------------------------------
+    # Download corresponding artifact (= dataset) from W&B
+    # TODO: maybe implement a main() with arguments to pass which artifact to download
+    '''
+    dataset_name = "Paired_with_Negatives"
+    artifact = wandb.use_artifact(f"{dataset_name}:latest")
+    BLABLABLA_table = artifact.get(f"{dataset_name}_table.table.json")
+    dataset_dir = artifact.download()
+    '''
+    
+    # for testing: 
+    # dataset_dir = "./samples.tsv"
+
+
+    # Initialize dataset
+    dataset = Paired()
+
+    SEQ_MAX_LENGTH = dataset.get_max_length()
+    print(f"this is SEQ_MAX_LENGTH: {SEQ_MAX_LENGTH}")
+
+
+    train_data, test_data = random_split(dataset, (0.8, 0.2))
+
+    # create samplers
+    train_sampler = RandomSampler(train_data)
+    test_sampler = SequentialSampler(test_data)
+
+    # Create data loaders.
+    train_dataloader = DataLoader(
+        train_data,
+        batch_size=BATCH_SIZE,
+        sampler=train_sampler,
+        num_workers=NUM_WORKERS,
+        collate_fn=pad_collate,
+    )
+    test_dataloader = DataLoader(
+        test_data,
+        batch_size=1,
+        sampler=test_sampler,
+        num_workers=NUM_WORKERS,
+        collate_fn=pad_collate,
+    )
+
+    # ---------------------------------------------------------------------------------
+    # model 
+    # ---------------------------------------------------------------------------------
+    model = ModelV1(SEQ_MAX_LENGTH)
+
+    # ---------------------------------------------------------------------------------
+    # training
+    # ---------------------------------------------------------------------------------
+    # Initialize loggers
+    # wandb_logger = WandbLogger(project="YourProjectName")
+    tensorboard_logger = TensorBoardLogger("tb_logs", name="ModelV1")
+
+    # Initialize a simple model checkpoint callback to save your models
+    
+    checkpoint_callback = ModelCheckpoint(
+        monitor='val_loss',
+        dirpath='./model_checkpoints',
+        filename='ModelV1-{epoch:02d}-{val_loss:.2f}',
+        save_top_k=3,
+        mode='min',
+    )
+    
+
+    # Initialize the trainer
+    trainer = pl.Trainer(
+        max_epochs=EPOCHS,
+        # logger=[wandb_logger, tensorboard_logger],
+        logger=[tensorboard_logger],
+        callbacks=[checkpoint_callback],
+        # accelerator="gpu" if torch.cuda.is_available() else "cpu",  # Adjust based on your setup
+        accelerator="gpu",
+        log_every_n_steps=1,
+    )
+
+    # Train the model
+    trainer.fit(model, train_dataloader, test_dataloader)
+
+    # Close W&B run
+    # wandb_logger.experiment.finish()
+
+    # ---------------------------------------------------------------------------------
+    # save model
+    # ---------------------------------------------------------------------------------
+    torch.save(model.state_dict(), MODEL_OUT)
+    print(f"Saved PyTorch Model State to {MODEL_OUT}")
+    
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/generateEmbeddings.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import numpy as np
+import gc
+import argparse
+import re
+import torch 
+from transformers import pipeline, T5Tokenizer, T5EncoderModel
+
+
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print("Using device: {}".format(device))
+
+
+#@title Load encoder-part of ProtT5 in half-precision. { display-mode: "form" }
+# Load ProtT5 in half-precision (more specifically: the encoder-part of ProtT5-XL-U50 in half-precision)
+transformer_link = "Rostlab/prot_t5_xl_half_uniref50-enc"
+print("Loading: {}".format(transformer_link))
+model = T5EncoderModel.from_pretrained(transformer_link)
+gc.collect()
+if device==torch.device("cpu"):
+    print("Casting model to full precision for running on CPU ...")
+model.to(torch.float32) # only cast to full-precision if no GPU is available
+model = model.to(device)
+model = model.eval()
+tokenizer = T5Tokenizer.from_pretrained(transformer_link, do_lower_case=False, legacy=True)
+
+'''
+1. load model and all these things 
+2. load corresponding df 
+3. create embeddings 
+4. store it ad .npz 
+5. call garbage collector
+    import gc
+
+    then after model instansiating: 
+    gc.collect()
+'''
+
+def load_data(file_name): 
+    base_path = "../data/customDatasets/negative_samples/"
+    df = pd.read_csv(base_path+file_name, sep="\t")
+
+    return df
+
+def process_batch(processed_seqs):
+    # Extract just the processed sequences for tokenization
+    sequences = [seq[1] for seq in processed_seqs]
+    ids = tokenizer.batch_encode_plus(sequences, add_special_tokens=True, padding="longest", return_tensors="pt")
+    input_ids = ids['input_ids'].to(device)
+    attention_mask = ids['attention_mask'].to(device)
+    
+    with torch.no_grad():
+        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+    
+    last_hidden_states = outputs.last_hidden_state
+    
+    # Now, return embeddings mapped to the original sequence
+    embeddings = {}
+    for i, (original_seq, _) in enumerate(processed_seqs):
+        seq_len = len(original_seq)
+        valid_embeddings = last_hidden_states[i,:seq_len]
+        per_protein_embedding = valid_embeddings.mean(dim=0)        
+        embedding = per_protein_embedding.cpu().numpy()
+        embeddings[original_seq] = embedding  # Use original sequence as key
+
+    return embeddings
+    
+
+if __name__ == "__main__":
+    # Create the parser
+    parser = argparse.ArgumentParser(description="Generate embeddings for protein sequences.")
+
+    # Add the arguments
+    parser.add_argument('file_name', type=str, help="The file name of the dataset.")
+    parser.add_argument('column_name', type=str, help="The column name containing the sequences.")
+    parser.add_argument('--prefix', type=str, default="embeddings_", help="Prefix for the output file.")
+
+    # Execute the parse_args() method
+    args = parser.parse_args()
+
+    df = load_data(args.file_name)
+    sequences = set(df[args.column_name].to_list())
+
+
+    processed_sequences = [(sequence, " ".join(list(re.sub(r"[UZOB]", "X", sequence)))) for sequence in sequences]
+
+    batch_size = 128
+    sequence_to_embedding = {}
+
+    # Batch processing with a dictionary, using original sequences as keys
+    for i in range(0, len(processed_sequences), batch_size):
+        batch_sequences = processed_sequences[i:i+batch_size]
+        batch_embeddings = process_batch(batch_sequences)
+        sequence_to_embedding.update(batch_embeddings)
+
+    to_path = "../data/Embeddings/"
+    file_name = args.prefix + "embeddings.npz"
+    np.savez(to_path+file_name, **sequence_to_embedding)
+
+
